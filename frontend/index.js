9ac8bfa31549117e0384fcf5576d05a1e7b38df9
Brandon Weaver <a7xncob@gmail.com>
2020-03-08 19:26:37 -0400

    added enemy class

--- frontend/index.js
+++ frontend/index.js
@@ -7,6 +7,8 @@ const background = new Background(game.context, 0, 0, 1280, 720, 0, 1, "assets/b
 
 const player = new Player(game.context, 600, 660, 40, 40, 0, 0, "assets/player");
 
+const enemies = [];
+
 function update() {
 	game.clear();
 	game.frame++;
@@ -22,4 +24,13 @@ function update() {
 	player.respondTo(keyboard);
 	player.move();
 	player.update();
+
+	if (game.intervalReached(250)) {
+		enemies.push(new Enemy(game.context, Math.floor(Math.random() * 1160) + 40, -40, 40, 40, 0, 1, "assets/player"))
+	}
+
+	for (const enemy of enemies) {
+		enemy.move();
+		enemy.update();
+	}
 }
---
+++ frontend/source/enemy.js
@@ -0,0 +1,13 @@
+class Enemy extends Ship {
+	constructor(context, x, y, width, height, xSpeed, ySpeed, texturePath) {
+		super(context, x, y, width, height, xSpeed, ySpeed, texturePath);
+	}
+
+	update() {
+		this.context.save();
+		this.context.translate(this.x + this.width / 2, this.y + this.height / 2);
+		this.context.rotate(180 * Math.PI / 180);
+		this.context.drawImage(this.texture, this.width / -2, this.height / -2, this.width, this.height);
+		this.context.restore();
+	}
+}
